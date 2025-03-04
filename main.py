b3465ce75e71414a3f7ef2e4ed9f46ffa21d9b3e
Shon Qwezon <shonzbso@gmail.com>
2025-03-04 21:36:31 +0300

    chore: add env for tokens

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from src.roles import owner, user
+from src.utils import setup_logger
+
+logger = setup_logger(__name__)
+
+
+async def main():
+    dp = Dispatcher()
+    dp.include_routers(owner, user)
+
+    bot = Bot(
+        token=os.getenv("TOKEN_BOT"),
+        default=DefaultBotProperties(parse_mode="html"),
+    )
+
+    logger.info("Старт бота")
+    await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
-    pass
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Работа приложения прервана")
+    except Exception as ex:
+        logger.critical(ex)
